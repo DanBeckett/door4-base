1d7c3c306753a677b864122d59a300f87fb1d300
Dan Beckett <dan.beckett@door4.com>
2017-04-11 17:12:06 +0100

    continued work on views for archive pages and some more of the standard blocks

---
+++ core/themes/egopowerplus/js/egopowerplus.js
@@ -0,0 +1,15 @@
+jQuery(window).load(function() {
+	// The review slider
+	if(jQuery('div.review_slider').length > 0) { 
+		jQuery('div.review_slider').flexslider({
+			animation: "slide",
+			animationLoop: true,
+			controlNav: false,
+			directionNav: true,
+			slideshowSpeed: 8000,
+			animationSpeed: 800,
+			prevText: "&#xf104;",
+			nextText: "&#xf105;",
+		});
+	};
+});
